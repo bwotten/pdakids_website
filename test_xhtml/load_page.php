4b0643dc96edc3047eeb0b421ea1352a971ff3e2
bwotten <bwotten@email.wme.edu>
2016-11-11 09:42:39 -0500

    Test on the file_get_contents

--- test_xhtml/load_page.php
+++ test_xhtml/load_page.php
@@ -6,7 +6,7 @@ if(!$_POST['page']) die("0");
 
 $page = (int)$_POST['page'];
 
-if(file_exists('pages/page_'.$page.'.html'))
+if(file_exists('pages/page_'.6.'.html'))
 echo file_get_contents('pages/page_'.$page.'.html');
 
 else echo 'There is no such page!';
