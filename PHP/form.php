0d858743cb67985fb487801d160e9ae14bfa3dc6
andres <ajarana@email.wm.edu>
2016-11-23 00:28:04 -0500

    Library updates and backup files

--- PHP/form.php
+++ PHP/form.php
@@ -1,53 +1,54 @@
 <?php
 
-	//if(!empty($_POST['submit']))
-	//{
-		$patientName=$_POST['patientName'];           //HTML element references
-		$parentName=$_POST['parentName'];
-		$relationship=$_POST['relationship'];
-		$insurance=$_POST['patientInsurance'];
-		$dayPhone=$_POST['dayPhone'];
-		$eveningPhone=$_POST['eveningPhone'];
-		$email=$_POST['email'];
-		$discovery=$_POST['discovery'];
-		$description=$_POST['description'];
+	$patientName=substr($_POST['patientName'],0,75);           //HTML element references
+	$parentName=substr($_POST['parentName'],0,75);
+	$relationship=substr($_POST['relationship'],0,75);
+	$insurance=substr($_POST['patientInsurance'],0,75);
+	$dayPhone=substr($_POST['dayPhone'],0,75);
+	$eveningPhone=substr($_POST['eveningPhone'],0,75);
+	$email=substr($_POST['email'],0,75);
+	$discovery=substr($_POST['discovery'],0,75);
+	$description=substr($_POST['description'],0,75);
 
-		require('libs/FPDF/fpdf.php');              //where lib file is stored
+	require('libs/FPDF/fpdf.php');              //where lib file is stored
 
-		$pdf=new FPDF();                            //constructs pdf based on user inputs
-		$pdf->AddPage();
-		$pdf->SetFont("Arial", "B", 16);
-		$pdf->Cell(50,10,"Patient Name:",1,0);
-		$pdf->Cell(140,10,$patientName,1,1);
+	$pdf=new FPDF();                            //constructs pdf based on user inputs
+	$pdf->AddPage();
+	$pdf->SetFont("Arial", "B", 16);
 
-		$pdf->Cell(50,10,"Parent Name:",1,0);
-		$pdf->Cell(140,10,$parentName,1,1);
+	$pdf->Cell(0,30,"New Patient Information",0,1,"C");
 
-		$pdf->Cell(50,10,"Relationship:",1,0);
-		$pdf->Cell(140,10,$relationship,1,1);
+	$pdf->Cell(60,10,"Patient Name:",1,0);
+	$pdf->Cell(130,10,$patientName,1,1);
 
-		$pdf->Cell(50,10,"Patient Insurance:",1,0);
-		$pdf->Cell(140,10,$insurance,1,1);
+	$pdf->Cell(60,10,"Parent Name:",1,0);
+	$pdf->Cell(130,10,$parentName,1,1);
 
-		$pdf->Cell(50,10,"Day Phone:",1,0);
-		$pdf->Cell(140,10,$dayPhone,1,1);
+	$pdf->Cell(60,10,"Relationship:",1,0);
+	$pdf->Cell(130,10,$relationship,1,1);
 
-		$pdf->Cell(50,10,"Evening Phone:",1,0);
-		$pdf->Cell(140,10,$eveningPhone,1,1);
+	$pdf->Cell(60,10,"Patient Insurance:",1,0);
+	$pdf->Cell(130,10,$insurance,1,1);
+
+	$pdf->Cell(60,10,"Day Phone:",1,0);
+	$pdf->Cell(130,10,$dayPhone,1,1);
+
+	$pdf->Cell(60,10,"Evening Phone:",1,0);
+	$pdf->Cell(130,10,$eveningPhone,1,1);
 	
-		$pdf->Cell(50,10,"Email:",1,0);
-		$pdf->Cell(140,10,$email,1,1);
+	$pdf->Cell(60,10,"Email:",1,0);
+	$pdf->Cell(130,10,$email,1,1);
 	
-		$pdf->Cell(50,10,"Discovery:",1,0);
-		$pdf->Cell(140,10,$discovery,1,1);
+	$pdf->Cell(60,10,"Discovery:",1,0);
+	$pdf->Cell(130,10,$discovery,1,1);
 
-		$pdf->Cell(50,10,"Description:",1,0);
-		$pdf->Cell(140,10,$description,1,1);
+	$pdf->Cell(60,20,"Description:",1);
+	$pdf->MultiCell(130,10,$description,1);
 	
-		$filename='/var/www/html/pdakids/patientInfo/NewPatient.pdf';  //where pdf is stored on the web server
-		$pdf->Output($filename, 'F');                                  //saves pdf
+	$filename='/var/www/html/pdakids/patientInfo/NewPatient.pdf';  //where pdf is stored on the web server
+	$pdf->Output($filename, 'F');   //saves pdf
 
-	//}
+                               
 
 	require 'libs/PHPMailer/PHPMailerAutoload.php';
 
@@ -57,18 +58,18 @@
 	$m->SMTPAuth = true;
 	//$m->SMTPDebug = 2;      //spits out connection info for debugging
 
-	$m->Host = 'smtp.live.com';               //to send email from pda server
-	$m->Username = 'testajarana@outlook.com';    //actual gmail username
-	$m->Password = 'testpassword1234';		   //actual gmail password
+	$m->Host = 'smtp.sendgrid.net';               //sendgrid 
+	$m->Username = 'ajarana';    //sendgrid username
+	$m->Password = 'testpassword1';		   //sendgrid password
 	$m->SMTPSecure = 'tls';
 	$m->Port = 587;
 
-	$m->From = 'testajarana@outlook.com';
-	$m->FromName = 'Andres Arana';
+	//$m->From = 'smtp.sendgrid.net';
+	$m->FromName = 'PDA Kids';
 	$m->addAddress('testajarana@outlook.com', 'Andres Arana');
 	$m->addAddress('testajarana@gmail.com', 'Andres Arana');
 	   //can also add bcc and more addresses
-	$m->addCC('motten@pdakids.com', 'Maureen');
+
 	$m->addCC('ajarana@email.wm.edu', 'Andres');
 	
 
@@ -78,6 +79,7 @@
 	$m->addAttachment('/var/www/html/pdakids/patientInfo/NewPatient.pdf', 'NewPatient.pdf');
 
 	$m->send();
+	unlink('/var/www/html/pdakids/patientInfo/NewPatient.pdf');
 
 	http_response_code(204);    //prevents script from opening on the browser
 
